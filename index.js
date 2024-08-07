332f98d2e31f777f16176a0610086e444eb0bb36
Tunahan Karakaya <bstunahan@gmail.com>
2024-08-07 13:42:19 +0200

    added env file

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
-const port = 3000
+require('dotenv').config();
+
 
 app.get('/test-api', (req, res) => {
   res.send('Hello World!')
 })
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Server listening on port ${port}`);
+});
